0a11c930c41f03659a69a00a568ac5d60739ba7f
Rasmus P <djrasmusp@users.noreply.github.com>
2025-04-20 21:13:01 +0200

    add app config

--- app.config.ts
+++ app.config.ts
@@ -1,1 +1,10 @@
-export default defineAppConfig({})
+import { field, group } from '@nuxt/content/preview'
+
+export default defineAppConfig({
+    appConfig: {
+        parent: group({
+            title: 'UI',
+            description: 'UI Components',
+        })
+    }
+})
