531dd1d21fd5d7a21d66d7697e7aef61c6581805
Rasmus P <djrasmusp@users.noreply.github.com>
2025-04-20 21:15:50 +0200

    clean appconfig

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,2 @@
-import { field, group } from '@nuxt/content/preview'
-
 export default defineAppConfig({
-    appConfig: {
-        parent: group({
-            title: 'UI',
-            description: 'UI Components',
-        })
-    }
 })
