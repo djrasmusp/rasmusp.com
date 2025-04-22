c43a7d84b5593a3cab66652d98dea73d1c4031f9
Rasmus P <djrasmusp@users.noreply.github.com>
2025-04-22 21:12:50 +0200

    wip

--- app/components/content/Experience.vue
+++ app/components/content/Experience.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
 const { data} = await useAsyncData('experience', () => {
-  return queryCollection('experience').all()
+  return queryCollection('experience').order('startDate', 'DESC').all()
 })
+
+function getYear(date: Date | null | undefined){
+  if(!date) return 'Present';
+
+  return new Date(date).getFullYear();
+}
 </script>
 
 <template>
 <div>
-  <pre>{{ data }}</pre>
+  <div v-for="item in data ">
+    <span>{{ getYear(item.startDate)}} - {{ getYear(item.endDate) }} <strong>{{ item.title}}, {{ item.company }} - {{ item.location}}</strong></span>
+  </div>
 </div>
 </template>
