cf7fb14cbed322fbc400da49977159f9d2167242
Rasmus P <djrasmusp@users.noreply.github.com>
2025-04-22 18:58:02 +0200

    wip

--- app/components/content/Skills.vue
+++ app/components/content/Skills.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
   defineProps<{
     categories: [{
-      title: string,
-      items: string[]
+      category: string,
+      items: [{ title: string}]
     }]
   }>()
 </script>
 
 <template>
   <div v-for="(category, index) in categories" :key="index">
-    <h2>{{ category.title }}</h2>
+    <h2>{{ category.category }}</h2>
     <div v-for="(item, index) in category.items" :key="index">
-      {{ item }}
+      {{ item.title }}
     </div>
 
   </div>
