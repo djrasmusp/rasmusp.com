d24b9f42dfe147e51830079e5ae16d07f2abf51d
Rasmus P <djrasmusp@users.noreply.github.com>
2025-04-22 18:41:24 +0200

    wip

--- app/components/content/HomeFeature.vue
+++ app/components/content/HomeFeature.vue
@@ -1,24 +1,14 @@
 <template>
-  <div class="flex items-start gap-3">
-    <div class="flex items-center justify-center border rounded-lg p-1.5">
-      <UIcon :name="icon" />
-    </div>
-    <div class="flex flex-col">
-      <h3 class="font-semibold">
-        <ContentSlot name="title" />
-      </h3>
-      <span>
-        <ContentSlot name="description" />
-      </span>
-    </div>
+  <div>
+    <h3>{{ title }}</h3>
   </div>
 </template>
 
 <script setup lang="ts">
 defineProps({
-  icon: {
+  title: {
     type: String,
-    default: 'i-ph-cursor-click',
+    default: 'Default title'
   },
 })
 </script>
