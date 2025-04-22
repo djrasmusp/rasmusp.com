493966c38f6412738084f51800680b516b67b727
Rasmus P <djrasmusp@users.noreply.github.com>
2025-04-22 18:31:12 +0200

    wip

---
+++ components/content/Skills.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  defineProps<{
+    categories: [{
+      title: string,
+      items: string[]
+    }]
+  }>()
+</script>
+
+<template>
+  <div v-for="(category, index) in categories" :key="index">
+    <h2>{{ category.title }}</h2>
+    <div v-for="(item, index) in category.items" :key="index">
+      {{ item }}
+    </div>
+
+  </div>
+</template>
