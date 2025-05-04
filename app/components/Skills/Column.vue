9f89b09b0f60e885fdebbf2a18bffb33679ec849
Rasmus P <djrasmusp@users.noreply.github.com>
2025-05-04 22:52:58 +0200

    add better padding

--- app/components/Skills/Column.vue
+++ app/components/Skills/Column.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="not-first:border-t border-dashed border-neutral-200 md:not-first:border-t-0 md:not-first:border-l lg:px-8"
+    class="not-first:border-t p-8 md:px-4 border-dashed border-neutral-200 md:not-first:border-t-0 md:not-first:border-l lg:px-8"
   >
     <slot />
   </div>
